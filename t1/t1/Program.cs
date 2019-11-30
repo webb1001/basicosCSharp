608ad522453446c7245395f0adedcc8e21b7d5a2
webb1001 <58349946+webb1001@users.noreply.github.com>
2019-11-30 09:51:24 -0600

    bool comparacionY= true && true; var comparacionO= true || false; bool comparacionIgual= comparacionY == comparacionO; bool valorNegado = ! comparacionIgual;

--- t1/t1/Program.cs
+++ t1/t1/Program.cs
@@ -31,6 +31,21 @@ namespace t1
             double resultadoPotencia = Math.Pow(78, 2);
             double resultadoRaiz = Math.Sqrt(144);
             Console.ReadLine();
+
+            int primerNumero = 48;
+            int segundoNumero = 12;
+            primerNumero += segundoNumero;
+            primerNumero -= segundoNumero;
+            primerNumero *= segundoNumero;
+            primerNumero /= segundoNumero;
+            primerNumero %= segundoNumero;
+            primerNumero++;
+            primerNumero--;
+
+            bool comparacionY = true && true;
+            var comparacionO = true || false;
+            bool comparacionIgual = comparacionY == comparacionO;
+            bool valorNegado = !comparacionIgual;
         }
     }
 }
