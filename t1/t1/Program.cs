4ed79e60620257607ce1c9262403efa520aad976
webb1001 <58349946+webb1001@users.noreply.github.com>
2019-11-30 09:43:55 -0600

    Se trabaja en operaciones asignación en C#

--- t1/t1/Program.cs
+++ t1/t1/Program.cs
@@ -7,6 +7,29 @@ namespace t1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //Console.ReadLine();
+
+            int añoActual = Convert.ToInt32("2019");
+            double numeroPI = 3.141592;
+            Console.WriteLine("Valores:{0} -- {1}", añoActual, numeroPI);
+            decimal montoFactura = 94593.34m + 0.07m;
+            float impuestoVenta = 9239.04f;
+            Console.WriteLine("Valores:{0} -- {1}", montoFactura, impuestoVenta);
+
+            char letraInicial = 'P';
+            string nombreCurso = letraInicial + "rogramación II";
+            Console.WriteLine("Este es el curso {0}", nombreCurso);
+            bool boolVerdadero = true;
+            bool boolFalso = false;
+            Console.WriteLine("Variables boleanas:{0} y {1}", boolVerdadero, boolFalso);
+
+            double resultadoSuma = 540 + -18.5;
+            float resultadoResta = 3458.35f - 334.34f;
+            decimal resultadoMultiplicacion = 23.423m * 3423m;
+            decimal resultadoDivision = 239.42m / 0.3m;
+            decimal resultadoResiduo = 2321.423m % 23m;
+            double resultadoPotencia = Math.Pow(78, 2);
+            double resultadoRaiz = Math.Sqrt(144);
             Console.ReadLine();
         }
     }
